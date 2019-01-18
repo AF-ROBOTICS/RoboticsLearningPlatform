8e4489eabf433ef4624b8f73b82922e4199f6c90
MacBook <smbeyer88@gmail.com>
2019-01-18 12:40:03 -0700

    added edge detection using line sensors.

--- code/lineSensor/lineSensor.ino
+++ code/lineSensor/lineSensor.ino
@@ -13,20 +13,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float outputLine_L = analogRead(lineL);
+  // float outputLine_L = analogRead(lineL);
   float outputLine_C = analogRead(lineC);
-  float outputLine_R = analogRead(lineR);
+  // float outputLine_R = analogRead(lineR);
 
-  Serial.print("Left: ");
-  Serial.print(outputLine_L);
-  Serial.print(" ");
+//  Serial.print("Left: ");
+//  Serial.print(outputLine_L);
+//  Serial.print(" ");
 
   Serial.print("Center: ");
-  Serial.print(outputLine_C);
-  Serial.print(" ");
+  Serial.println(outputLine_C);
+//  Serial.print(" ");
 
-  Serial.print("Right: ");
-  Serial.println(outputLine_R);
+//  Serial.print("Right: ");
+//  Serial.println(outputLine_R);
 
   delay(1000); // print every second
 }
